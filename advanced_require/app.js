ab901844a14d9ad26e95398904e0e8df9791da25
trebek1 <trebek08@gmail.com>
2016-10-05 12:21:47 -0700

    added some notes

--- advanced_require/app.js
+++ advanced_require/app.js
@@ -1,3 +1,24 @@
+
+// NOTES 
+
+// if you require a file it looks for js file automatically or what you explicitly defined it as 
+// this code is in the node require javascript core 
+
+// if none exist, looks for directory with same name 
+//  -> then looks for index file in that directory 
+
+// requiring a json file -> javascript can interpret it and use the variables in it
+
+// module.exports is an {} that can be added to 
+
+// can set it to a new object with the 'new' keyword. The new keyword just creates a new object. 
+
+// objects are passed by reference 
+
+// once a module.exports is required, it is cached and calling again will call same one again. 
+
+// HERE ARE SOME DESIGN PATTERNS
+
 var greet = require('./greet1');
 greet();
 
