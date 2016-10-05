36f0a33b50c31b38e4c84faa97cfb5ea0c1edb00
trebek1 <trebek08@gmail.com>
2016-10-05 13:52:56 -0700

    added a config to make code easier to debug

--- emitter/app.js
+++ emitter/app.js
@@ -1,16 +1,17 @@
 var Emitter = require('events');
 //var Emitter = require('./emitter'); 
+var greet = require('./config').events.GREET;
 
 var emtr = new Emitter(); 
 
-emtr.on('greet', function(){
+emtr.on(greet, function(){
 	console.log("Somewhere, someone said hello");
 })
 
-emtr.on('greet', function(){
+emtr.on(greet, function(){
 	console.log("Greeting Occured!");
 })
 
 console.log("Hello");
-emtr.emit('greet');
+emtr.emit(greet);
 
