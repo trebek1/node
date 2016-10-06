a7c8c927ce775e61e0340f10375aff5ff07ba0ae
trebek1 <trebek08@gmail.com>
2016-10-06 13:29:51 -0500

    converted emitter to ES6, explaination of class..extends and super relating to EventEmitter.call(this) and util.inherits

--- inheritance/advanced_inheritance.js
+++ inheritance/advanced_inheritance.js
@@ -1,17 +1,33 @@
+'use strict'
+
 var EventEmitter = require('events'); 
 var util = require('util');
 
-function Greetr(){
-	EventEmitter.call(this); // like super 
-	this.greeting = 'hello world!';
+// extends eventEmitter is the same as EventEmitter.call(this) with super() .... Boom! 
+
+class Greetr extends EventEmitter {
+	
+	constructor(){
+		super()
+		this.greeting = 'hello world';
+	}
+	greet(data){
+		console.log(this.greeting + ': ' + data);
+		this.emit('greet',data);
+	}
 }
 
-util.inherits(Greetr,EventEmitter);
+// function Greetr(){
+// 	EventEmitter.call(this); // -> elim with super 
+// 	this.greeting = 'hello world!';
+// }
 
-Greetr.prototype.greet = function(data){
-	console.log(this.greeting + ': ' + data);
-	this.emit('greet',data);
-}
+//util.inherits(Greetr,EventEmitter); -> elim with extends
+
+// Greetr.prototype.greet = function(data){
+// 	console.log(this.greeting + ': ' + data);
+// 	this.emit('greet',data);
+// }
 
 var greeter1 = new Greetr(); 
 greeter1.on('greet', function(data){
