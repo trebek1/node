d97d70beba24e5bf4193b207006a8f4c89b5c734
trebek1 <trebek08@gmail.com>
2016-10-11 12:55:38 -0700

    sever vs client test

--- angular/app.js
+++ angular/app.js
@@ -3,12 +3,24 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var people = [
+		{
+			name: 'alex'
+		},
+		{
+			name: 'tim'
+		},
+		{
+			name: 'bryan'
+		}
+	]
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	
-	res.render('index');
+	res.render('index', {serverPeople: people});
 	
 });
 
--- angular/public/js/app.js
+++ angular/public/js/app.js
@@ -4,16 +4,6 @@ angular.module('TestApp')
 	.controller('MainController', ctrlFunc);
 
 function ctrlFunc(){
-	this.message = 'Hello'; 
-	this.people = [
-		{
-			name: 'alex'
-		},
-		{
-			name: 'tim'
-		},
-		{
-			name: 'bryan'
-		}
-	]
+	
+	this.people = clientPeople;	
 }
