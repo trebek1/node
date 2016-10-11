eb9f452a1a02b3d9df7542daf42f5655b20d0081
trebek1 <trebek08@gmail.com>
2016-10-10 23:49:50 -0700

    added templates using ejs to express

--- express-basics/index.js
+++ express-basics/index.js
@@ -3,8 +3,25 @@ var app = express();
 
 var port = process.env.PORT || 3000; 
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req,res){
-	res.send('<html><head></head><body><h1> Hello World</h1></body></html>');
+	res.render('index')
 });
 
+app.get('/person/:id', function(req,res){
+	res.render('person', {id: req.params.id})
+});
+
+// app.get('/person/:id', function(req,res){
+// 	res.send('<html><head></head><body><h1> Person ' + req.params.id + '</h1></body></html>');
+// });
+
+app.get('/api', function(req,res){
+	res.json({first: 'alex', last: 'john'});
+})
+
 app.listen(port);
+
