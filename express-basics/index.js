ae433aeefbb9c05e201ac5f0119b94de0b1218cd
trebek1 <trebek08@gmail.com>
2016-10-11 00:21:33 -0700

    routing in express

--- express-basics/index.js
+++ express-basics/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000; 
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var jsonParser = bodyParser.json(); 
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -12,8 +16,23 @@ app.get('/', function(req,res){
 });
 
 app.get('/person/:id', function(req,res){
-	res.render('person', {id: req.params.id})
+	res.render('person', {id: req.params.id, Qstr: req.query.qstr})
 });
+
+
+app.post('/person', urlencodedParser, function(req,res){
+	res.send('thanks');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+	
+});
+
+app.post('/personjson', jsonParser, function(req, res){
+	res.send('thanks for the json even though its not that great');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+})
+
 
 // app.get('/person/:id', function(req,res){
 // 	res.send('<html><head></head><body><h1> Person ' + req.params.id + '</h1></body></html>');
